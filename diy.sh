38fc7816f19caa62f910ef9c0cb1d787c3c09dbe
sheno-Tyes <42202881+sheno-Tyes@users.noreply.github.com>
2020-11-06 21:43:32 +0800

    Initial commit

---
+++ diy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+#mkdir package/base-files/files/config
+#echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+#echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+#./scripts/feeds update -a && ./scripts/feeds install -a
